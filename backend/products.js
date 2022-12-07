1aaa81b85315b963101ead2e9f8e82587862ffbb
suraj53 <surajcsgo@gmail.com>
2022-12-07 17:25:16 +0530

    tenth commit

---
+++ backend/products.js
@@ -0,0 +1,33 @@
+const Products = [
+    {
+        id:1,
+        name:"iPhone 12 Pro",
+        desc:"6.1 inches display",
+        price:999,
+        image:"https://res.cloudinary.com/djmpi5jus/image/upload/v1670322527/iphone12pro_oe81rw.jpg"
+    },
+    {
+        id:2,
+        name:"iPhone 12 Pro Max",
+        desc:"6.5 inches display",
+        price:1250,
+        image:"https://res.cloudinary.com/djmpi5jus/image/upload/v1670322527/iphone12promax_q1xqxa.jpg"
+    },
+    {
+        id:3,
+        name:"Galaxy S22 Ultra",
+        desc:"7.1 inches display",
+        price:1499,
+        image:"https://res.cloudinary.com/djmpi5jus/image/upload/v1670322527/s22Ultra_gw0vq2.jpg"
+    },
+    {
+        id:4,
+        name:"Realme 9 Pro",
+        desc:"6.5 inches display",
+        price:899,
+        image:"https://res.cloudinary.com/djmpi5jus/image/upload/v1670401647/phones/realme-9-pro_rqm0xv.jpg"
+    }
+]
+
+
+module.exports = Products;
